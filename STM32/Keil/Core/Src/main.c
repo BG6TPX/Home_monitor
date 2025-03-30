9bc291678f3cd42fd814c566513d85dfad78bf52
zyZhang <1450665003@qq.com>
2025-03-30 15:52:45 +0800

    fix(uart): update stm32 uart1

--- STM32/Keil/Core/Src/main.c
+++ STM32/Keil/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "usart.h"
 #include "usb.h"
 #include "gpio.h"
 
@@ -88,6 +89,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USB_PCD_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
